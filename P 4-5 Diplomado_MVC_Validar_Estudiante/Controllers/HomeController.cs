38e40c8c5bb6ac9a432fa80457e73968df303dea
Juan Esteban Gonzalez <e-gonzalez@outlook.com>
2020-08-18 12:55:38 -0400

    Agregar archivos de proyecto.

---
+++ P 4-5 Diplomado_MVC_Validar_Estudiante/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using P_4_5_Diplomado_MVC_Validar_Estudiante.Models;
+
+namespace P_4_5_Diplomado_MVC_Validar_Estudiante.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Estudiante est = new Estudiante();
+            return View(est);
+        }
+        [HttpPost]
+        public ActionResult Index(Estudiante est)
+        {
+            if (ModelState.IsValid)
+                return View("Correcto");
+            else
+                return View(est);
+        }
+        public ActionResult Correcto()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ P 4-5 Diplomado_MVC_Validar_Estudiante/Models/Estudiante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace P_4_5_Diplomado_MVC_Validar_Estudiante.Models
+{
+    public class Estudiante
+    {
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        [MinLength(3,ErrorMessage ="El nombre del estudiante debe tener al menos 3 carateres")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [MinLength(3, ErrorMessage = "El apellido del estudiante debe tener al menos 2 carateres")]
+        public string Apellidos { get; set; }
+
+        [Range(14,65, ErrorMessage ="La edad del estudiante debe estar entre 14 y 65")]
+        public int Edad { get; set;  }
+
+        [EmailAddress(ErrorMessage ="debe ingresar un email válido")]
+        public string Email { get; set; }
+
+        [RegularExpression("[MmFf]", ErrorMessage ="Solo puede ingresar una M=Masculino o F=Feminino")]
+        public string Genero { get; set; }
+
+        [RegularExpression("[CcSsUuDd]", ErrorMessage ="Solo puede ingresar una C=Casado, S=Soltero, U=Union Libre o D=Divorciado")]
+        public string EstadoCivil { get; set; }
+    }
+}
